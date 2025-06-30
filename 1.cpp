354bd29064d113325b0b9fdde4036957fe47757d
PENDOSOS <ivanpuchkin@gmail.com>
2025-06-30 16:13:26 +0300

    removed debug print from 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,6 @@
 // Approximate time - 1 hour
 
 #include "1.h"
-#include <iostream>
-#include <cmath>
 
 #define BIT_SEQUENCE_LENGTH 32
 
@@ -12,9 +10,7 @@ const uint32_t invertBitSequence(const uint32_t inputNumber)
 	for (int i = 0; i < BIT_SEQUENCE_LENGTH; i++)
 	{
 		int invertedBit = (inputNumber >> i) & 1;
-		std::cout << invertedBit;
 		result |= invertedBit << (BIT_SEQUENCE_LENGTH - 1 - i);
 	}
-	std::cout << std::endl << result;
 	return result;
 }
